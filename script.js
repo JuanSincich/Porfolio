bc1f4393a2f6310c22b2a000a4ab3d80c94b16c8
Dimaroch <juansincich.design@gmail.com>
2023-09-14 13:29:41 -0300

    listo-para_js

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 /* ----------------PAGINA CERTIFICACIONES------------------ */
 
-/* const images = document.querySelectorAll(".clickable");
-
-document.addEventListener("click", (event) => {
-  images.forEach((image) => {
-    if (image.classList.contains("expanded") && !image.contains(event.target)) {
-      image.classList.remove("expanded");
-    }
-  });
-});
-
-images.forEach((image) => {
-  image.addEventListener("click", (event) => {
-    event.stopPropagation();
-    if (image.classList.contains("expanded")) {
-      image.classList.remove("expanded");
-    } else {
-      images.forEach((otherImage) => {
-        if (otherImage.classList.contains("expanded")) {
-          otherImage.classList.remove("expanded");
-        }
-      });
-      image.classList.add("expanded");
-    }
-  });
-}); */
-
 const images = document.querySelectorAll(".clickable");
 
 document.addEventListener("click", (event) => {
@@ -69,5 +43,23 @@ panels.forEach((panel) => {
       removeActiveClasses();
       panel.classList.add("active");
     }
+  });
+});
+/* ----------------go-top-button------------------ */
+
+const goTopButton = document.querySelector(".go-top-button");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    goTopButton.classList.add("mostrar");
+  } else {
+    goTopButton.classList.remove("mostrar");
+  }
+});
+
+goTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
 });
